efb8512ddaca29dbc8aa0abaa8eb35c676f506c5
kingpaul <king.pulgares@achievewithoutborders.com>
2023-01-31 17:04:15 +0800

    replace draftjs to react-quill

--- src/pages/post/Create.js
+++ src/pages/post/Create.js
@@ -1,38 +1,65 @@
-import { Button, Card, CardBody, CardHeader, Col, Row } from "reactstrap";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import ReactSelect from "react-select";
 
-import RichText from "../../components/Form/RichTextEditor/RichText";
-
-import Avatar from "../../assets/default-avatar.png";
+import { Button, Col, Row } from "reactstrap";
+import "react-quill/dist/quill.snow.css";
 
 export const Create = () => {
+  const [form, setForm] = useState("");
+
+  const toolbarOptions = [
+    { size: ["small", false, "large", "huge"] },
+    "bold",
+    "italic",
+    "underline",
+    { list: "ordered" },
+    { list: "bullet" },
+    "code-block",
+    // "image",
+    "clean",
+  ];
+
+  const groupOptions = [
+    { value: "THS1", label: "Thesis Writing 1" },
+    { value: "THS2", label: "Thesis Writing 2" },
+    { value: "CSPRAC", label: "CS Practicum" },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   return (
     <Col className="create">
       <h1 className="">Create post</h1>
       <p className="text-muted border-bottom mb-3 pb-2">
-        Choose group/class, type formatted texts or upload photos
+        Type formatted post, Select groups/classes.
       </p>
       <Row className="mx-lg-auto">
-        <Col lg="7" className="position-relative mb-3">
-          <RichText />
-        </Col>
-        <Col lg="5">
-          <Card>
-            <CardHeader
-              tag="h5"
-              className="d-flex justify-content-between text-white card-header"
-            >
-              Photos <span>1/5</span>
-            </CardHeader>
-            <CardBody>
-              <Row className="m-auto">
-                <Col xs="4" className="position-relative">
-                  <i className="fa-solid fa-circle-xmark position-absolute end-0 top-0 me-2"></i>
-                  <img src={Avatar} className="img-thumbnail mb-2" alt="" />
-                </Col>
-              </Row>
-              <Button className="w-100 main-btn">Add Image 1/5</Button>
-            </CardBody>
-          </Card>
+        <Col md="8" className="mb-3 mx-auto">
+          <ReactQuill
+            className="bg-white"
+            theme="snow"
+            placeholder="Type your post here"
+            modules={{
+              toolbar: toolbarOptions,
+            }}
+            value={form}
+            onChange={(e) => setForm(e)}
+          />
+          <div className="d-flex gap-2 mt-2">
+            <ReactSelect
+              className="w-100"
+              isMulti="true"
+              placeholder="Choose a group or class"
+              options={groupOptions}
+            />
+            <Button onClick={handleSubmit} className="main-btn ">
+              Post
+            </Button>
+          </div>
         </Col>
       </Row>
     </Col>
