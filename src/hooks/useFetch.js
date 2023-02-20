c31f4c057a70e5bca5d5414df5cf8cbe8dff328c
kingpaul <king.pulgares@achievewithoutborders.com>
2023-02-20 18:20:48 +0800

    fix/redirection after post

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -27,7 +27,7 @@ export const useFetch = () => {
 
     if (response.ok) {
       dispatch({ type, payload: json });
-      navigate("/feed");
+      navigate("/feed/");
     } else {
       setError(json.error.message);
       setTimeout(() => setError(null), "4500");
